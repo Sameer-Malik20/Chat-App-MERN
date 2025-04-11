ab330562e17d63a63b2f803efff7ac68b00b05ab
Sameer-Malik20 <sameermalik63901@gmail.com>
2025-04-11 14:38:39 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,8 @@ const mongoose = require("mongoose")
 
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(cors({ origin: "https://localhost:3000", credentials: true }));
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
