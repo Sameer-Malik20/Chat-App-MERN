1e229ac3669229f9168f9b4fb2bf8fdf66798c7c
Sameer-Malik20 <sameermalik63901@gmail.com>
2025-04-12 21:36:11 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -11,10 +11,7 @@ app.use(cors());
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.error("Error connecting to MongoDB", err));
 
